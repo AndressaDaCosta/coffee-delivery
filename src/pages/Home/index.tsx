f56f017082b2d792cdbe8203cae2b57deaa504ea
Andressa Da Costa <andressadacostan@gmail.com>
2023-03-08 00:03:16 -0300

    home-intro

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,153 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import CoffeeImage from '../../assets/CoffeeDelivery.png'
+
+import {
+	IntroContainer,
+	IntroInfoContainer,
+	IntroTitle,
+	IntroItems,
+	ImageBackground,
+	DivContainer
+} from './style'
+
+// Coffees
+import ExpressoTradicional from '../../assets/Coffees/expresso-tradicional.svg'
+import ExpressoAmericano from '../../assets/Coffees/expresso-americano.svg'
+import ExpressoCremoso from '../../assets/Coffees/expresso-cremoso.svg'
+import ExpressoGelado from '../../assets/Coffees/expresso-gelado.svg'
+import CafeComLeite from '../../assets/Coffees/cafe-com-leite.svg'
+import Latte from '../../assets/Coffees/latte.svg'
+import Capuccino from '../../assets/Coffees/capuccino.svg'
+import Macchiato from '../../assets/Coffees/macchiato.svg'
+import Mocaccino from '../../assets/Coffees/mocaccino.svg'
+import ChocolateQuente from '../../assets/Coffees/chocolate-quente.svg'
+import Cubano from '../../assets/Coffees/cubano.svg'
+import Havaiano from '../../assets/Coffees/havaiano.svg'
+import Arabe from '../../assets/Coffees/arabe.svg'
+import Irlandes from '../../assets/Coffees/irlandes.svg'
+
 export function Home() {
-	return <h1>Home</h1>
+	return (
+		<DivContainer>
+			<ImageBackground />
+			<IntroContainer>
+				<IntroInfoContainer>
+					<IntroTitle>
+						<h1>
+							Encontre o café perfeito para qualquer hora do dia
+						</h1>
+						<p>
+							Com o Coffee Delivery você recebe seu café onde
+							estiver, a qualquer hora
+						</p>
+					</IntroTitle>
+
+					<IntroItems>
+						<div>
+							<ShoppingCart
+								size={22}
+								weight="fill"
+							/>
+							<p>Compra simples e segura</p>
+						</div>
+						<div>
+							<Package
+								size={22}
+								weight="fill"
+							/>
+							<p>Embalagem mantém o café intacto</p>
+						</div>
+						<div>
+							<Timer
+								size={22}
+								weight="fill"
+							/>
+							<p>Entrega rápida e rastreada</p>
+						</div>
+						<div>
+							<Coffee
+								size={22}
+								weight="fill"
+							/>
+							<p>O café chega fresquinho até você</p>
+						</div>
+					</IntroItems>
+				</IntroInfoContainer>
+
+				<img
+					alt=""
+					src={CoffeeImage}
+				/>
+			</IntroContainer>
+			<section>
+				<h2>Nossos cafés: 
+				</h2>
+				<br></br>
+			</section>
+
+			<div>
+				<img
+					alt=""
+					src={ExpressoTradicional}
+				/>
+
+				<img
+					alt=""
+					src={ExpressoAmericano}
+				/>
+				<img
+					alt=""
+					src={ExpressoCremoso}
+				/>
+				<img
+					alt=""
+					src={ExpressoGelado}
+				/>
+				<img
+					alt=""
+					src={CafeComLeite}
+				/>
+				<img
+					alt=""
+					src={Latte}
+				/>
+				<img
+					alt=""
+					src={Capuccino}
+				/>
+				<img
+					alt=""
+					src={Macchiato}
+				/>
+				<img
+					alt=""
+					src={Mocaccino}
+				/>
+				<img
+					alt=""
+					src={ChocolateQuente}
+				/>
+				<img
+					alt=""
+					src={Cubano}
+				/>
+				<img
+					alt=""
+					src={Mocaccino}
+				/>
+				<img
+					alt=""
+					src={Havaiano}
+				/>
+				<img
+					alt=""
+					src={Arabe}
+				/>
+				<img
+					alt=""
+					src={Irlandes}
+				/>
+			</div>
+		</DivContainer>
+	)
 }
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,1 +1,55 @@
 import styled from 'styled-components'
+
+export const DivContainer = styled.div`
+	position: relative;
+`
+export const ImageBackground = styled.img`
+	background: url(https://raw.githubusercontent.com/AndressaDaCosta/coffee-delivery/88a80b8165ace4e76564e4dcb9c201e5ba25be9b/src/assets/Background.svg);
+	filter: blur(70px);
+	width: 100%;
+	height: 24rem;
+	position: absolute;
+	top: 0;
+`
+
+export const IntroContainer = styled.section`
+	display: flex;
+	justify-content: center;
+	gap: 3.5rem;
+	margin-top: 5.87rem;
+	margin-bottom: 6.75rem;
+	position: relative;
+`
+
+export const IntroInfoContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 4.125rem;
+`
+
+export const IntroTitle = styled.div`
+	gap: 1rem;
+	h1 {
+		font-family: 'Baloo 2', sans-serif;
+		font-size: 3rem;
+		font-weight: 800;
+		line-height: 130%;
+		color: ${(props) => props.theme['base-title']};
+	}
+	p {
+		font-size: 1.25rem;
+		line-height: 130%;
+		color: ${(props) => props.theme['base-subtitle']};
+	}
+`
+
+export const IntroItems = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 2.5rem;
+	div {
+		display: flex;
+		align-items: center;
+		gap: 0.75rem;
+	}
+`
