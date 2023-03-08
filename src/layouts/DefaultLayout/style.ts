4f19770f904444016907d31ed71aeaee18e2ab7a
Andressa Da Costa <andressadacostan@gmail.com>
2023-03-07 23:32:06 -0300

    default-layout-update

--- src/layouts/DefaultLayout/style.ts
+++ src/layouts/DefaultLayout/style.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
 	width: 100%;
-	max-width: 70rem;
+	max-width: 90rem;
 	margin: 0 auto;
+	padding: 1.25rem;
 	/* max-width: 70rem;= 1120 / 1440 */
 `
