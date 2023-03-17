ab1b0dd247244f82799a34d26774ee164001004f
Andressa Da Costa <andressadacostan@gmail.com>
2023-03-17 11:10:19 -0300

    start-checkout-page:construction:

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,61 @@
+import { AddressForm, CheckoutContainer, PaymentOptions } from './styles'
+
 export function Checkout() {
-	return <h1>Checkout</h1>
+	return (
+		<CheckoutContainer>
+			<AddressForm>
+				<input
+					type="text"
+					placeholder="CEP"
+				/>
+				<input
+					type="text"
+					placeholder="Rua"
+				/>
+				<input
+					type="text"
+					placeholder="Número"
+				/>
+				<input
+					type="text"
+					placeholder="Complemento"
+				/>
+				<input
+					type="text"
+					placeholder="Bairro"
+				/>
+				<input
+					type="text"
+					placeholder="Cidade"
+				/>
+				<input
+					type="text"
+					placeholder="UF"
+				/>
+			</AddressForm>
+			<PaymentOptions>
+				<label>
+					<input
+						type="button"
+						name="opcao-pagamento"
+					/>
+					Cartão de Crédito
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="opcao-pagamento"
+					/>
+					Cartão de Débito
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="opcao-pagamento"
+					/>
+					Boleto Bancário
+				</label>
+			</PaymentOptions>
+		</CheckoutContainer>
+	)
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,1 +1,37 @@
 import styled from 'styled-components'
+
+export const CheckoutContainer = styled.div`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`
+
+export const AddressForm= styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 500px;
+  margin-bottom: 20px;
+  input {
+    margin-bottom: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+  }
+`
+export const PaymentOptions= styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 500px;
+  margin-bottom: 20px;
+  label {
+    font-size: 16px;
+    margin-right: 10px;
+  }
+`
